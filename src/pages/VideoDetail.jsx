e2f3981290cfcd8ac30843e2bb5fd5f57e9d0446
Ari <ari89.web@gmail.com>
2023-11-21 20:29:56 +0900

    "domain edit"

--- src/pages/VideoDetail.jsx
+++ src/pages/VideoDetail.jsx
@@ -1,19 +1,21 @@
 import React from "react";
 import { useLocation } from "react-router-dom";
-import styles from './VideoDetail.module.css';
+import styles from "./VideoDetail.module.css";
 import ChannelInfo from "../components/ChannelInfo";
 import ChannelPlaylist from "../components/ChannelPlaylist";
 
 export default function VideoDetail() {
-  const { state : {video}} = useLocation();
-  const { title,channelId, channelTitle, description } = video.snippet;
+  const {
+    state: { video },
+  } = useLocation();
+  const { title, channelId, channelTitle, description } = video.snippet;
   return (
     <div className={styles.container}>
       <div className={styles.videobox}>
         <iframe
           id="player"
           type="text/html"
-          src={`http://www.youtube.com/embed/${video.id}`}
+          src={`https://www.youtube.com/embed/${video.id}`}
           title={title}
         />
         <p className={styles.title}>{title}</p>
